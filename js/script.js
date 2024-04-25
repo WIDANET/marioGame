d690366b559f9e807358a8ceadf5b3dfd40e9d7e
WIDANET <willianlps99@outlook.com>
2024-04-24 22:18:01 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
-mario.classList.add('jump');
+    mario.classList.add('jump');
+
+    setTimeout(() =>{
+
+    mario.classList.remove('jump');
+
+    }, 500);
+
 }
 
-document.addEventListener('click', jump);
+const loop = setInterval(() => {
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    console.log(marioPosition);
+    
+    if(pipePosition <= 120){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
