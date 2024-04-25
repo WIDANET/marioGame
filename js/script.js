186c58dbce9b61a6781486a67a8736322f1fa90c
WIDANET <willianlps99@outlook.com>
2024-04-24 23:10:28 -0300

    song update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
+const audioPrincipal = new Audio("../media/mario-music.mp3")
+const  marioMorre = new Audio ("../media/dead-mario.mp3")
+const marioJump = new Audio("../media/mario-pulo.mp3")
+ 
+
 const jump = () => {
     mario.classList.add('jump');
+    marioJump.play()
 
     setTimeout(() =>{
 
@@ -13,16 +19,27 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
+
+    audioPrincipal.play()
     
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-    console.log(marioPosition);
     
-    if(pipePosition <= 120){
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = '/images/game-over.png';
+        mario.style.width = '80px'
+        mario.style.marginLeft = '40px'
+        audioPrincipal.pause()
+        marioMorre.play();
+
+        clearInterval(loop);
     }
 
 }, 10);
